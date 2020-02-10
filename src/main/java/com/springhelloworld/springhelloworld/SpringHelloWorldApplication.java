597e3603eae3665ab68d8be587dc16c84f984c4d
Marcelo Carbonera <marcelocarbonera@live.com>
2020-02-10 19:52:28 -0200

    Dominios Cidade e Estado

--- src/main/java/com/springhelloworld/springhelloworld/SpringHelloWorldApplication.java
+++ src/main/java/com/springhelloworld/springhelloworld/SpringHelloWorldApplication.java
@@ -1,8 +1,12 @@
 package com.springhelloworld.springhelloworld;
 
 import com.springhelloworld.springhelloworld.domain.Categoria;
+import com.springhelloworld.springhelloworld.domain.Cidade;
+import com.springhelloworld.springhelloworld.domain.Estado;
 import com.springhelloworld.springhelloworld.domain.Produto;
 import com.springhelloworld.springhelloworld.repositories.CategoriaRepository;
+import com.springhelloworld.springhelloworld.repositories.CidadeRepository;
+import com.springhelloworld.springhelloworld.repositories.EstadoRepository;
 import com.springhelloworld.springhelloworld.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -17,6 +21,10 @@ public class SpringHelloWorldApplication implements CommandLineRunner {
     CategoriaRepository categoriaRepository;
     @Autowired
     ProdutoRepository produtoRepository;
+    @Autowired
+    EstadoRepository estadoRepository;
+    @Autowired
+    CidadeRepository cidadeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringHelloWorldApplication.class, args);
@@ -24,6 +32,7 @@ public class SpringHelloWorldApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        /* RELACIONAMENTO CATEGORIA PRODUTO */
         Categoria cat1 = new Categoria(null, "Teste 1");
         Categoria cat2 = new Categoria(null, "Teste 2");
 
@@ -40,5 +49,19 @@ public class SpringHelloWorldApplication implements CommandLineRunner {
 
         categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
         produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
+        /* RELACIONAMENTO CIDADE ESTADO */
+        Estado est1 = new Estado(null,"Minas Gerais");
+        Estado est2 = new Estado(null,"São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2,c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1,est2));
+        cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
     }
 }
