d8a576e3c307b8d1ef1e4076b23cc253371ceed3
Marcelo Carbonera <marcelocarbonera@live.com>
2020-02-10 16:59:22 -0200

    retorno json

--- src/main/java/com/springhelloworld/springhelloworld/resources/CategoriaResource.java
+++ src/main/java/com/springhelloworld/springhelloworld/resources/CategoriaResource.java
@@ -1,17 +1,28 @@
 package com.springhelloworld.springhelloworld.resources;
 
+import com.springhelloworld.springhelloworld.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value="/categorias")
-public class CategoricalResource {
+public class CategoriaResource {
 
     //@RequestMapping(method= RequestMethod.GET)
     @GetMapping
-    public String listar() {
-        return "Rest Funcionando";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1,"Teste1");
+        Categoria cat2 = new Categoria(2,"Teste2");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
