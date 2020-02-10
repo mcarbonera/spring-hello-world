2745bd9a5d0fd58fdfdf098f101d2b62c49124ab
Marcelo Carbonera <marcelocarbonera@live.com>
2020-02-10 18:02:29 -0200

    operação de instanciação (inicialização do banco)

--- src/main/java/com/springhelloworld/springhelloworld/SpringHelloWorldApplication.java
+++ src/main/java/com/springhelloworld/springhelloworld/SpringHelloWorldApplication.java
@@ -1,11 +1,28 @@
 package com.springhelloworld.springhelloworld;
 
+import com.springhelloworld.springhelloworld.domain.Categoria;
+import com.springhelloworld.springhelloworld.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringHelloWorldApplication {
+public class SpringHelloWorldApplication implements CommandLineRunner {
+    @Autowired
+    CategoriaRepository repo;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringHelloWorldApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Teste 1");
+        Categoria cat2 = new Categoria(null, "Teste 2");
+
+        repo.saveAll(Arrays.asList(cat1,cat2));
     }
 }
