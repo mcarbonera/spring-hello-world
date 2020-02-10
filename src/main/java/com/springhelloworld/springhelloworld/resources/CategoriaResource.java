bc75d110a56e5193025e59efce91f2493a8e7284
Marcelo Carbonera <marcelocarbonera@live.com>
2020-02-10 17:52:51 -0200

    Repository e Service

--- src/main/java/com/springhelloworld/springhelloworld/resources/CategoriaResource.java
+++ src/main/java/com/springhelloworld/springhelloworld/resources/CategoriaResource.java
@@ -1,10 +1,10 @@
 package com.springhelloworld.springhelloworld.resources;
 
 import com.springhelloworld.springhelloworld.domain.Categoria;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.springhelloworld.springhelloworld.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,17 +12,12 @@ import java.util.List;
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-
+    @Autowired
+    private CategoriaService service;
     //@RequestMapping(method= RequestMethod.GET)
-    @GetMapping
-    public List<Categoria> listar() {
-        Categoria cat1 = new Categoria(1,"Teste1");
-        Categoria cat2 = new Categoria(2,"Teste2");
-
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-
-        return lista;
+    @GetMapping(value="/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
