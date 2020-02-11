acf7df8354499e281400ee5e03d535b0368e0833
Marcelo Carbonera <marcelocarbonera@live.com>
2020-02-11 14:21:02 -0200

    Substituir Json[...]Managers por JsonIgnore.

--- src/main/java/com/springhelloworld/springhelloworld/domain/Pagamento.java
+++ src/main/java/com/springhelloworld/springhelloworld/domain/Pagamento.java
@@ -1,6 +1,6 @@
 package com.springhelloworld.springhelloworld.domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.springhelloworld.springhelloworld.enums.EstadoPagamento;
 
 import javax.persistence.*;
@@ -16,7 +16,7 @@ public abstract class Pagamento implements Serializable {
     private Integer id;
     private Integer estado;
 
-    @JsonBackReference
+    @JsonIgnore
     @OneToOne
     @JoinColumn(name = "pedido_id")
     @MapsId
--- src/main/java/com/springhelloworld/springhelloworld/domain/Produto.java
+++ src/main/java/com/springhelloworld/springhelloworld/domain/Produto.java
@@ -1,6 +1,5 @@
 package com.springhelloworld.springhelloworld.domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
@@ -18,7 +17,7 @@ public class Produto implements Serializable {
     private Double preco;
 
     //@Transient
-    @JsonBackReference
+    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "PRODUTO_CATEGORIA",
             joinColumns = @JoinColumn(name = "produto_id"),
