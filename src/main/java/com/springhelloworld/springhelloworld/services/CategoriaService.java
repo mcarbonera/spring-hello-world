623aa55bc782eadf216f1869df66735e8ab0bacc
Marcelo Carbonera <marcelocarbonera@live.com>
2020-02-11 16:04:43 -0200

    DELETE com excessão BAD_RESQUEST

--- src/main/java/com/springhelloworld/springhelloworld/services/CategoriaService.java
+++ src/main/java/com/springhelloworld/springhelloworld/services/CategoriaService.java
@@ -1,9 +1,11 @@
 package com.springhelloworld.springhelloworld.services;
 
 import com.springhelloworld.springhelloworld.domain.Categoria;
+import com.springhelloworld.springhelloworld.exceptions.DataIntegrityException;
 import com.springhelloworld.springhelloworld.exceptions.ObjectNotFoundException;
 import com.springhelloworld.springhelloworld.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -28,6 +30,15 @@ public class CategoriaService {
     public Categoria update(Categoria obj) {
         find(obj.getId());
         return categoriaRepository.save(obj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            categoriaRepository.deleteById(id);
+        } catch(DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+        }
     }
 }
 
